196932da02a5b84bb59b63ceb6aa2ae19457dc15
Vladimir <barrierofdust@gmail.com>
2021-12-07 13:50:10 +0300

    Add Functional template method

---
+++ ApplicationWithAllPatterns/TemplateMethod/FunctionalTemplateMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    public static class FunctionalTemplateMethod
+    {
+        private static void Start() => Console.WriteLine("--------Start opperation---------");
+        private static void End() => Console.WriteLine("---------------End---------------");
+        public static void Make(Action action)
+        {
+            Start();
+            action();
+            End();
+            Console.WriteLine();
+        }
+    }
+}
